5ce0f80d5ed4fb42707198cf3f61bf91bd1b9e0f
rabbanirizwan <rabbani_rizwan@yahoo.com>
2019-05-25 17:50:21 +0500

    seccond

---
+++ script.js
@@ -0,0 +1,9 @@
+function S()
+{
+    console.log("sdasdasd");
+    document.querySelector(".spin").style.display="none";
+    document.querySelector(".spinner1").innerHTML+=`<div class="spinner-border text-primary" role="status">
+  <span class="sr-only">Loading...</span>
+</div>`;
+
+}
