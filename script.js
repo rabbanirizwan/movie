2d600d671ad580c912a294de4fc29a9ea2aad788
rabbanirizwan <rabbani_rizwan@yahoo.com>
2019-05-26 22:39:09 +0500

    eight

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function S()
 {
     console.log("sdasdasd");
     document.querySelector(".spin").style.display="none";
-    document.querySelector(".spinner1").innerHTML+=`<div class="spinner-border text-primary" role="status">
+    document.querySelector(".spinner1").innerHTML+=`<div style="width:100px;height:100px" class="spinner-border text-primary" role="status">
   <span class="sr-only">Loading...</span>
 </div>`;
 
